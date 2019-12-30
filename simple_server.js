57db7fa71a2886f7fb2bc8d42bf879e9d6a040d9
phuongnguyen891991 <phuongnguyen891991@gmail.com>
2019-12-30 23:13:07 +0700

    Details: Create mini chat but could not running for now

--- simple_server.js
+++ simple_server.js
@@ -9,13 +9,26 @@ http.createServer(function (req, res) {
 console.log('Server running at http://127.0.0.1:3000/');
 */
 
+
+/*Create simple web server*/
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer();
 //Setting up an event listener for request
 server.on('request', function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.writeHead(200, {'Content-Type': 'image/ipg'});
+  fs.createReadStream('./191010_nature.jpg').pipe(res);
+  // res.end('Hello World\n');
 })
 server.listen(3000);
 console.log('Server running at http://localhost:3000/');
+
+/*Streaming data*/
+// var stream = fs.createReadStream('./191010_nature.jpg').pipe(res);
+// stream.on('data', function (chunk) {
+//   console.log(chunk)
+// })
+// stream.on('end', function () {
+// console.log('finished')
+// })
